43d608b25cfd01ecc31edfd13729f7f9249826cc
StephenDelaCruz <stephendelacruzz@gmail.com>
2024-09-24 14:06:42 +1200

    slay images showing and design epic

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -2,13 +2,13 @@ import eventsData from '../../../_data/events.json';
 
 const EventsPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-start h-screen bg-gray-100 pt-20"> {/* Adjust pt-20 as needed */}
       <h1 className="text-3xl font-semibold mb-4">Upcoming Events</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {eventsData.events.map((event) => (
           <div key={event.id} className="bg-white shadow-md rounded-lg p-4">
             <img
-              src={`/images/${event.image}`} // Adjust the path based on your project structure
+              src={`/img/${event.image}`} // Adjust the path based on your project structure
               alt={event.title}
               className="w-full h-48 object-cover rounded-lg mb-2"
             />
@@ -21,5 +21,6 @@ const EventsPage = () => {
     </div>
   );
 };
+
 
 export default EventsPage;
